d291734750326edac4e89359ccaba30080cba009
Georg <georg.goering-extern@cloudogu.com>
2025-08-01 11:26:49 +0200

    #1 update various operator files

--- release_args.sh
+++ release_args.sh
@@ -9,16 +9,8 @@ update_versions_modify_files() {
   valuesYAML=k8s/helm/values.yaml
   componentPatchTplYAML=k8s/helm/component-patch-tpl.yaml
 
-  ./.bin/yq -i ".controllerManager.image.tag = \"${newReleaseVersion}\"" "${valuesYAML}"
+  ./.bin/yq -i ".manager.image.tag = \"${newReleaseVersion}\"" "${valuesYAML}"
   ./.bin/yq -i ".values.images.debugModeOperator |= sub(\":(([0-9]+)\.([0-9]+)\.([0-9]+)((?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))|(?:\+[0-9A-Za-z-]+))?)\", \":${newReleaseVersion}\")" "${componentPatchTplYAML}"
-
-  local chownInitImage
-  chownInitImage=$(./.bin/yq ".additionalImages.chownInitImage" "${valuesYAML}")
-  ./.bin/yq -i ".values.images.chownInitImage = \"${chownInitImage}\"" "${componentPatchTplYAML}"
-
-  local exporterImage
-  exporterImage=$(./.bin/yq ".additionalImages.exporterImage" "${valuesYAML}")
-  ./.bin/yq -i ".values.images.exporterImage = \"${exporterImage}\"" "${componentPatchTplYAML}"
 }
 
 update_versions_stage_modified_files() {
