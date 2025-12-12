0ac6cdfa1f8f1e4cfcecebda579ede6459479bbf
Laurin Friebe <laurin.friebe-extern@cloudogu.com>
2025-12-12 11:26:49 +0100

    Remove dogu restarts from debug mode operator

--- internal/loglevel/interfaces.go
+++ internal/loglevel/interfaces.go
@@ -2,19 +2,15 @@ package loglevel
 
 import (
 	"context"
+
 	"github.com/cloudogu/ces-commons-lib/dogu"
 	"github.com/cloudogu/cesapp-lib/core"
-	doguClient "github.com/cloudogu/k8s-dogu-lib/v2/client"
 	"github.com/cloudogu/k8s-registry-lib/config"
 )
 
 type DoguConfigRepository interface {
 	Get(context.Context, dogu.SimpleName) (config.DoguConfig, error)
 	Update(context.Context, config.DoguConfig) (config.DoguConfig, error)
-}
-
-type DoguRestarter interface {
-	Restart(ctx context.Context, doguName string) error
 }
 
 type DoguDescriptorGetter interface {
@@ -24,10 +20,5 @@ type DoguDescriptorGetter interface {
 type LogLevelHandler interface {
 	GetLogLevel(ctx context.Context, element any) (LogLevel, error)
 	SetLogLevel(ctx context.Context, element any, targetLogLevel LogLevel) error
-	Restart(ctx context.Context, name string) error
 	Kind() string
-}
-
-type DoguRestartInterface interface {
-	doguClient.DoguRestartInterface
 }
